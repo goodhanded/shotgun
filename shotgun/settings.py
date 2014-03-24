00cf282de35322fb3e34f92d5d4e9f30c4ce3f67
Keith Morris <keith.a.morris@gmail.com>
2014-03-24 14:14:11 -0400

    Updated database var

--- shotgun/settings.py
+++ shotgun/settings.py
@@ -101,7 +101,7 @@ WSGI_APPLICATION = 'shotgun.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(PROJECT_PATH, 'db.sqlite3'),
     }
 }
 
