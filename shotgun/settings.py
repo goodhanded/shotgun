298c1f83a6d74c4235135de10ecbbf3168f1a720
Keith Morris <keith.a.morris@gmail.com>
2014-03-24 14:11:58 -0400

    removed base_dirs setting

--- shotgun/settings.py
+++ shotgun/settings.py
@@ -10,15 +10,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 PROJECT_ROOT = os.path.dirname(__file__)
 PROJECT_PATH = os.path.dirname(PROJECT_ROOT)
-
-ADMINS = (
-    ('Keith Morris', 'keitham@live.unc.edu'),
-)
-
-MANAGERS = ADMINS
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
