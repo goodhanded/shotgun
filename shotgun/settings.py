ca5956e849965c9bf40aaaeb2f6b514068acc9d8
Keith Morris <keith.a.morris@gmail.com>
2014-03-25 10:22:49 -0400

    Updated project directory structure and settings file

--- shotgun/settings.py
+++ shotgun/settings.py
@@ -8,26 +8,24 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
 PROJECT_ROOT = os.path.dirname(__file__)
 PROJECT_PATH = os.path.dirname(PROJECT_ROOT)
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '8xsh%by4t5^tk+h$)0(hzl49f5jun2=a)hm*a!u^+3qvc7dqd%'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-# Application definition
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'America/New_York'
+
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -47,14 +45,13 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
 
 ROOT_URLCONF = 'shotgun.urls'
-
 WSGI_APPLICATION = 'shotgun.wsgi.application'
-
-
-# # Database
-# # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
 DATABASES = {
     'default': {
@@ -63,40 +60,16 @@ DATABASES = {
     }
 }
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'America/New_York'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
-
-MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media_root')
-MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'static_media')
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'uploads')
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/uploads/'
 
 STATICFILES_DIRS = (
-    # Put strings here like '/home/html/static' or  'C:/www/django/static'.
-    os.path.join(PROJECT_ROOT, 'static_media'),
-)
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates"
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, "templates"),
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
