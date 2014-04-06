7fd4a2ab1f60b87fc529b6b2d03b8ad82d639622
Keith Morris <keith.a.morris@gmail.com>
2014-04-05 23:08:23 -0400

    Removed easy_maps

--- drive/models.py
+++ drive/models.py
@@ -1,9 +1,1 @@
 from django.db import models
-from easy_maps.models import Address
-
-# class Ride(models.Model):
-
-#     fromAddress = models.ForeignKey('Address')
-#     toAddress = models.ForeignKey('Address')
-
-
